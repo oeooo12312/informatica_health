8c57d2fb539614cedf6d6ca9e90cf979f75aec3c
pohja1999 <98691869+pohja1999@users.noreply.github.com>
2025-03-12 16:43:53 +0800

    Adding some skeleton to the codebase

---
+++ src/Authentication/Authentication.jsx
@@ -0,0 +1,11 @@
+
+function Authentication() {
+
+    /**
+     * This authentication should have the following conditions :
+     *      1. Voice-based 
+     *      2. Have both sign-up and login features
+     */
+}
+
+export default Authentication;
---
+++ src/Payment/Payment.jsx
@@ -0,0 +1,11 @@
+
+function Payment() {
+
+    /**
+     * This payment should fulfill the following conditions : 
+     *      1. Voice-based
+     *      2. Allow users to make payments for their medical bills 
+     */
+}
+
+export default Payment;
