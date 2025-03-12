58b80539d87cc2f5cddc5233ea39b4833d0b7fc1
pohja1999 <98691869+pohja1999@users.noreply.github.com>
2025-03-12 17:32:04 +0800

    Restructured directory

--- src/views/shared/Authentication/Authentication.jsx
+++ src/views/shared/Authentication/Authentication.jsx
@@ -1,10 +1,10 @@
 
-function Authentication() {
-
+function Authentication(user) {
     /**
      * This authentication should have the following conditions :
-     *      1. Voice-based 
+     *      1. Voice-based for patient, visual for the rest.
      *      2. Have both sign-up and login features
+     *      3. General for all patient/doctor/pharmacy
      */
 }
 
